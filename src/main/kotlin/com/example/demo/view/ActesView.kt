a6907200f8628605138dd7e6a355f1d1594b78d4
forntoh <thomasforntoh@gmail.com>
2020-07-03 19:43:46 +0100

    reflect selection in Actes/Medications form

--- src/main/kotlin/com/example/demo/view/ActesView.kt
+++ src/main/kotlin/com/example/demo/view/ActesView.kt
@@ -4,10 +4,13 @@ import com.example.demo.app.Styles
 import com.example.demo.controller.ActesController
 import com.example.demo.data.model.ActeModel
 import com.example.demo.data.model.MedicationModel
+import com.example.demo.data.model.toMedicationModel
 import com.example.demo.utils.defaultPadding
 import javafx.beans.binding.Bindings
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.geometry.Orientation
+import javafx.scene.control.ComboBox
+import javafx.scene.control.TableView
 import tornadofx.*
 
 class ActesView : View("Actes/Medications") {
@@ -18,6 +21,10 @@ class ActesView : View("Actes/Medications") {
     private val isMedication = SimpleBooleanProperty(false)
 
     private val actesController: ActesController by inject()
+
+    var tableOfActes: TableView<ActeModel> by singleAssign()
+    var tableOfMedications: TableView<MedicationModel> by singleAssign()
+    var sectionsCombo: ComboBox<String> by singleAssign()
 
     override val root = splitpane {
 
@@ -45,7 +52,7 @@ class ActesView : View("Actes/Medications") {
                     hbox {
                         spacing = defaultPadding
                         field("Section") {
-                            combobox<String>(actesModel.synthesisSectionName) {
+                            sectionsCombo = combobox(actesModel.synthesisSectionName) {
                                 medicationModel.synthesisSectionName.bind(actesModel.synthesisSectionName)
                                 items = actesController.sections.map { it.name.value }.observable()
                                 isMedication.addListener { _, _, isTrue ->
@@ -56,8 +63,7 @@ class ActesView : View("Actes/Medications") {
                             }
                         }
                         field {
-                            checkbox("Medication") {
-                                bind(isMedication)
+                            checkbox("Medication", isMedication) {
                                 prefWidth = 250.0
                             }
                         }
@@ -80,6 +86,8 @@ class ActesView : View("Actes/Medications") {
                                 actesModel.rollback()
                                 medicationModel.warehouseStock.value = 0
                                 medicationModel.counterStock.value = 0
+                                tableOfActes.selectionModel.clearSelection()
+                                tableOfMedications.selectionModel.clearSelection()
                             }
                         }
                         button("Delete") {
@@ -109,15 +117,21 @@ class ActesView : View("Actes/Medications") {
 
         vbox {
             label("Actes").addClass(Styles.subheading)
-            tableview<ActeModel> {
+            tableOfActes = tableview {
                 items = actesController.actes
                 column(messages["id"], ActeModel::id)
                 column(messages["label"], ActeModel::name)
                 column(messages["price"], ActeModel::appliedAmount)
                 column(messages["price"], ActeModel::officialAmount)
+                selectionModel.selectedItemProperty().addListener { _, _, it ->
+                    if (selectionModel.selectedItem != null) {
+                        updateActeModel(it.toMedicationModel())
+                        isMedication.value = false
+                    }
+                }
             }
             label("Medications").addClass(Styles.subheading)
-            tableview<MedicationModel> {
+            tableOfMedications = tableview {
                 items = actesController.medications
                 column(messages["id"], MedicationModel::id)
                 column(messages["label"], MedicationModel::name)
@@ -126,11 +140,30 @@ class ActesView : View("Actes/Medications") {
 
                 column(messages["price"], MedicationModel::counterStock)
                 column(messages["price"], MedicationModel::warehouseStock)
+
+                selectionModel.selectedItemProperty().addListener { _, _, it ->
+                    if (selectionModel.selectedItem != null) {
+                        updateActeModel(it)
+                        isMedication.value = true
+                        medicationModel.counterStock.value = it.counterStock.value
+                        medicationModel.warehouseStock.value = it.warehouseStock.value
+                    }
+                }
             }
             spacing = defaultPadding
             paddingAll = defaultPadding
         }
 
         setDividerPositions(0.4)
+    }
+
+    private fun updateActeModel(it: MedicationModel) {
+        actesModel.id.value = it.id.value
+        actesModel.name.value = it.name.value
+        actesModel.officialAmount.value = it.officialAmount.value
+        actesModel.appliedAmount.value = it.appliedAmount.value
+        actesModel.synthesisSectionId.value = it.synthesisSectionId.value
+        actesModel.synthesisSectionName.value = it.synthesisSectionName.value
+        sectionsCombo.selectionModel.select(it.synthesisSectionName.value)
     }
 }
